307d3ca54d4b73cc2788bbca04d17afea1de4ba4
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2022-12-08 01:15:20 -0300

    function removerGrupo correção
    Remoção de bugs na função remover grupos
    criação de logs

--- _js/script.js
+++ _js/script.js
@@ -1,13 +1,21 @@
+// lista_grupo : contem o nome do grupo
+// lista_container_grupo : contem os elementos HTML para criação dos grupos sem os itens
+// lista_container_iten: contem os elementos HTML para criação dos itens em seus respectivos grupos
+
+// Cada grupo criado receberá um indice no array (1º grupo = recebe 0, 2º grupo recebe 1 ...), esse indice será o mesmo das nomenclaturas dos ids HTML
+// Na remoção de um grupo, o indice receberá vazio, não sendo apagado para não perder a sequencia de numeação da criação dos grupos
+
 var lista_grupo = Array();
 var lista_container_grupo = Array();
 var lista_container_itens = Array();
 var valor_htm = "";
 var item_html = "";
-var countgrupo = 1;
+var countgrupo = 0;
 
-// console.log(document.getElementById("spanum1").innerHTML);
+//console.log(document.getElementById(lista_container_itens[-1]));
 
 function adicionarGrupo() {
+    console.log("Adicionar Grupo");
     // recupera o nome do grupo no input 
     var grupo = document.getElementById("name").value.toUpperCase().trim();
 
@@ -16,6 +24,7 @@ function adicionarGrupo() {
     for (let i = 0; i < lista_grupo.length; i++) {
         if (lista_grupo[i] == grupo) {
             achou = true;
+            break;
         }
     }
 
@@ -24,17 +33,19 @@ function adicionarGrupo() {
         alert("valor existe");
 
     } else {
-        // se nao existir o valor digitado no array, verifica se o campo está vazio
+        // se não existir o valor digitado no array, verifica se o campo está vazio
         if (grupo != "") {
-
+            console.log("Valor Adicionado: " + grupo);
             // se o valor digitado for válido
-            // valor_htm vai receber os elementos e atributos HTML para a criação do contrner do grupo inserido
-            // Para cada id de cada elementos vai receber seu nome numerado iniciando com 1
+            // variável valor_htm vai receber os elementos e atributos HTML para a criação do contrner do grupo inserido
+            // Cada id de cada elementos vai receber seu valor finalizado com uma numeração para indicação do grupo
             // O grupo será inserido no elementos span com o id spangroup + contador
             valor_htm = "<div id='container" + countgrupo + "'><div id='namegroup" + countgrupo + "'class='group'><span id='spangroup" + countgrupo + "'>" + grupo + "</span><span id='spanum" + countgrupo + "' class='apaga'>" + countgrupo + "</span></div><p id='itens" + countgrupo + "'><ol id='olitens" + countgrupo + "'></ol></p><div class='form-inline'><input type='text' id='proxitem" + countgrupo + "' class='form-control mt-1' placeholder='Próximo Item'><a href='#' onclick='adicionarItens(" + countgrupo + ")'><i class='fa-solid fa-circle-up fa-lg ml-2'></i></a></div></div>";
+
+            // o countgrupo é uma variavel global e contantemente sendo complementado
             countgrupo++;
 
-        
+
             // adiciona o nome do grupo no array
             lista_grupo.push(grupo);
 
@@ -51,78 +62,87 @@ function adicionarGrupo() {
 
 
 function removerGrupo() {
+    console.log("Remover Grupo");
     var t = "";
     var achou = false;
 
     // recupera o nome do grupo no input
     var grupo = document.getElementById("name").value.toUpperCase().trim();
 
+    var num_grupo = -1;
 
     if (grupo != "") {
-        for (let i = 0; i < lista_container_grupo.length; i++) {
-            // No array lista_container_grupo contém os elementos e atributos HTML para a criação do contrner do grupo inserido e cada indice referencia-se a um grupo
-            // Utilizado o comando search() para realizar uma busca da string ( >nome do grupo<), se a busca for positiva retorna a posição da string, caso contrário retorna -1
-            // Caso exista o grupo no array o indice refenrente será apagado
-            if (lista_container_grupo[i].toString().search(">" + grupo + "<") >= 0) {
-                // lista_container_grupo.splice(i, 1);
-                lista_container_grupo[i] = "";
-                lista_container_itens[i + 1] = "";
+        console.log("Valor Digitado: " + grupo);
+
+        // verifica se o grupo inserido existe no array
+        for (let i = 0; i < lista_grupo.length; i++) {
+            if (lista_grupo[i] == grupo) {
+                console.log("valor " + grupo + " indice " + i + " encontrado");
                 achou = true;
+                num_grupo = i;
                 break;
             }
         }
 
-
+        // verifica se existe itens inseridos no grupo no qual será removido
+        //caso exista algum item, a remoção será negada
         if (achou) {
-            // Deleta do Array lista_grupo no nome do grupo caso a busca anterior for positiva
-            for (let i = 0; i < lista_grupo.length; i++) {
-                if (lista_grupo[i] == grupo) {
-                    // lista_grupo.splice(i, 1);
-                    lista_grupo[i] = "";
-                    break;
+            if (lista_container_itens[num_grupo] == "" || lista_container_itens[num_grupo] == undefined) {
+                console.log("Grupo sem itens - iniciar remoção");
+
+
+                lista_container_grupo[num_grupo] = "";
+                lista_grupo[num_grupo] = "";
+
+                // Remonta os elementos e atributos HTML para a criação do contrner do grupo após a remoção do grupo solicitado
+                for (let i = 0; i < lista_container_grupo.length; i++) {
+                    if (lista_container_grupo[i] != "") {
+                        t += lista_container_grupo[i];
+                    }
                 }
-            }
 
-            // Remonta os elementos e atributos HTML para a criação do contrner do grupo após a remoção do grupo solicitado
-            for (let i = 0; i < lista_container_grupo.length; i++) {
-                if(lista_container_grupo[i] != "") {
-                    t += lista_container_grupo[i];
+                document.getElementById('section').innerHTML = t;
+                document.getElementById("name").value = "";
+
+                // Remonta os elementos e atributos HTML dos itens de cada grupo após a remoção do grupo solicitado
+                for (let i = 0; i < lista_container_itens.length; i++) {
+                    if (lista_container_itens[i] != "") {
+                        document.getElementById("olitens" + i).innerHTML = lista_container_itens[i];
+                    }
                 }
+
+            } else {
+                alert("Grupo não pode ser removido, pois contém itens");
             }
-
-            // Remonta os elementos e atributos HTML dos itens de cada grupo após a remoção do grupo solicitado
-            for (let i = 0; i < lista_container_itens.length; i++) {
-                lista_container_itens[0] = "";
-                if(lista_container_itens[i] != "") {
-                    let v = lista_container_itens[i];
-                    document.getElementById("olitens" + i).innerHTML = v;
-                    console.log(document.getElementById("olitens" + i).innerHTML);
-                }
-            }
-
-            document.getElementById('section').innerHTML = t;
-            document.getElementById("name").value = "";
-
-
         } else {
-            alert("valor não existe");
+            alert("Valor não existe");
         }
     } else {
-        alert("valor não existe");
+        alert("O campo não foi preenchdo corretamente");
     }
 
 }
 
 
 
-// Adiciona itens com numeração do id e grupo como parâmetros 
+// Adiciona itens com numeração do indice do grupo como parâmetros 
 function adicionarItens(id) {
+
+    console.log("Adicionar Itens para: " + lista_grupo[id]);
+
     var nameitem = document.getElementById("proxitem" + id).value.toUpperCase().trim();
-    if(lista_container_itens.length > id && lista_container_itens[id] != ""){
-        lista_container_itens[id] += "<li>" + nameitem + "<a href='#' onclick='riscarItens(" + nameitem + ")'><i class='fa-solid fa-check fa-lg ml-2'></i></a><a href='#' onclick='apagarItens(" + nameitem + ")'><i class='fa-solid fa-trash ml-2'></i></a></li>";
+
+    if (nameitem != "") {
+        if (lista_container_itens.length > id && (lista_container_itens[id] != "" || lista_container_itens[id] != undefined)) {
+            lista_container_itens[id] += "<li>" + nameitem + "<a href='#' onclick='riscarItens(" + nameitem + ")'><i class='fa-solid fa-check fa-lg ml-2'></i></a><a href='#' onclick='apagarItens(" + nameitem + ")'><i class='fa-solid fa-trash ml-2'></i></a></li>";
+
+        } else {
+            lista_container_itens[id] = "<li>" + nameitem + "<a href='#' onclick='riscarItens(" + nameitem + ")'><i class='fa-solid fa-check fa-lg ml-2'></i></a><a href='#' onclick='apagarItens(" + nameitem + ")'><i class='fa-solid fa-trash ml-2'></i></a></li>";
+        }
     } else {
-        lista_container_itens[id] = "<li>" + nameitem + "<a href='#' onclick='riscarItens(" + nameitem + ")'><i class='fa-solid fa-check fa-lg ml-2'></i></a><a href='#' onclick='apagarItens(" + nameitem + ")'><i class='fa-solid fa-trash ml-2'></i></a></li>";
+        alert("O campo não foi preenchdo corretamente");
     }
+
     document.getElementById("olitens" + id).innerHTML = lista_container_itens[id];
     document.getElementById("proxitem" + id).value = "";
 
